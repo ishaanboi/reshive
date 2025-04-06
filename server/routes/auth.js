5725e757cddbb333202b02e74137e73a82f9fb13
ishaanboi <i.ishaanpant21@gmail.com>
2025-04-06 05:51:32 +0530

    fix ig

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,14 @@
+// server/routes/auth.js
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser } = require('../../controllers/authController');
+
+// Test route
 router.get('/test', (req, res) => {
     res.send('Auth route works!');
 });
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
 
+// Auth routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
